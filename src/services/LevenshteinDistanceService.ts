2f27dae027e21df85898b328320b889ac23cc8f0
Steven Liebregt <s.liebregt@outlook.com>
2020-12-11 11:38:18 +0100

    implemented copied levenshtein with max distance

--- src/services/LevenshteinDistanceService.ts
+++ src/services/LevenshteinDistanceService.ts
@@ -1,4 +1,15 @@
 export default class LevenshteinDistanceService {
+    _min = (d0: number, d1: number, d2: number, bx: number, ay: number) =>
+    {
+        return d0 < d1 || d2 < d1
+            ? d0 > d2
+                ? d2 + 1
+                : d0 + 1
+            : bx === ay
+                ? d1
+                : d1 + 1;
+    }
+
     canBeTransformedInMaxDistance = (source: string, target: string, maxDistance: number): boolean => {
         if (source === target) return true;
         if (source.length === 0) return target.length <= maxDistance;
@@ -14,20 +25,13 @@ export default class LevenshteinDistanceService {
 
         if (targetLength - sourceLength > maxDistance) return false;
 
-
-
-
-
-
-
-        // TODO: Not yet sure what this is used for
+        // TODO: Refactor and understand this code from: https://github.com/fonograph/js-levenshtein/blob/master/index.js
         while (sourceLength > 0 && (source.charCodeAt(sourceLength - 1) === target.charCodeAt(targetLength - 1))) {
             sourceLength--;
             targetLength--;
         }
 
-        // Ignore all the starting characters that are the same in both the source and the target.
-        let offset = 0;
+        var offset = 0;
 
         while (offset < sourceLength && (source.charCodeAt(offset) === target.charCodeAt(offset))) {
             offset++;
@@ -36,99 +40,75 @@ export default class LevenshteinDistanceService {
         sourceLength -= offset;
         targetLength -= offset;
 
-        if (sourceLength === 0 || targetLength < 3) return targetLength <= maxDistance; // TODO: <=  or <
-
-
-        // TODO: Figure this out nicely
-
-        let matrix: number[][] = [[]];
-        let i;
-        let j;
-
-        for (j = 0; j <= targetLength; j++) {
-            matrix[0][j] = j;
+        if (sourceLength === 0 || targetLength < 3) {
+            return targetLength <= maxDistance;
         }
 
-        for (i = 1; i <= sourceLength; i++) {
-            let c1: string;
-            let minJ: number;
-            let maxJ: number;
-            let colMin: number;
-            let next: number;
-            let prev: number;
+        var x = 0;
+        var y;
+        var d0;
+        var d1;
+        var d2;
+        var d3;
+        var dd;
+        var dy;
+        var ay;
+        var bx0;
+        var bx1;
+        var bx2;
+        var bx3;
+        var min;
 
-            c1 = source[i - 1];
+        var vector = [];
 
-            minJ = 1;
-            if (i > maxDistance) {
-                minJ = i - maxDistance;
-            }
-            maxJ = targetLength;
-            if (targetLength > maxDistance + i) {
-                maxJ = maxDistance + i;
-            }
-            colMin = Number.MAX_VALUE;
-            next = i % 2;
-            if (next == 1) {
-                prev = 0;
-            }
-            else {
-                prev = 1;
-            }
+        for (y = 0; y < sourceLength; y++) {
+            vector.push(y + 1);
+            vector.push(source.charCodeAt(offset + y));
+        }
 
-            if (matrix[next] === undefined) {
-                matrix[next] = [];
-            }
+        var len = vector.length - 1;
 
-            matrix[next][0] = i;
-            /* Loop over rows. */
-            for (j = 1; j <= targetLength; j++) {
-                if (j < minJ || j > maxJ) {
-                    /* Put a large value in there. */
-                    matrix[next][j] = maxDistance + 1;
-                }
-                else {
-                    let c2: string;
-
-                    c2 = target[j-1];
-                    if (c1 == c2) {
-                        /* The character at position i in word1 is the same as
-                           the character at position j in word2. */
-                        matrix[next][j] = matrix[prev][j-1];
-                    }
-                    else {
-                        /* The character at position i in word1 is not the
-                           same as the character at position j in word2, so
-                           work out what the minimum cost for getting to cell
-                           i, j is. */
-                        let del: number;
-                        let insert: number;
-                        let substitute: number;
-                        let minimum: number;
-
-                        del = matrix[prev][j] + 1;
-                        insert = matrix[next][j-1] + 1;
-                        substitute = matrix[prev][j-1] + 1;
-                        minimum = del;
-                        if (insert < minimum) {
-                            minimum = insert;
-                        }
-                        if (substitute < minimum) {
-                            minimum = substitute;
-                        }
-                        matrix[next][j] = minimum;
-                    }
-                }
-                /* Find the minimum value in the ith column. */
-                if (matrix[next][j] < colMin) {
-                    colMin = matrix[next][j];
+        for (; x < targetLength - 3;) {
+            bx0 = target.charCodeAt(offset + (d0 = x));
+            bx1 = target.charCodeAt(offset + (d1 = x + 1));
+            bx2 = target.charCodeAt(offset + (d2 = x + 2));
+            bx3 = target.charCodeAt(offset + (d3 = x + 3));
+            dd = (x += 4);
+            min = Infinity;
+            for (y = 0; y < len; y += 2) {
+                dy = vector[y];
+                ay = vector[y + 1];
+                d0 = this._min(dy, d0, d1, bx0, ay);
+                d1 = this._min(d0, d1, d2, bx1, ay);
+                d2 = this._min(d1, d2, d3, bx2, ay);
+                dd = this._min(d2, d3, dd, bx3, ay);
+                vector[y] = dd;
+                d3 = d2;
+                d2 = d1;
+                d1 = d0;
+                d0 = dy;
+                if (maxDistance >= 0) {
+                    min = Math.min(min, dd);
                 }
             }
-            if (colMin > maxDistance) {
-                // TODO: Not working correctly
-                /* All the elements of the ith column are greater than the
-                   maximum, so no match less than or equal to max can be
-                   found by looking at succeeding columns. */
+            if (maxDistance >= 0 && min > maxDistance) {
+                return false;
+            }
+        }
+
+        for (; x < targetLength;) {
+            bx0 = target.charCodeAt(offset + (d0 = x));
+            dd = ++x;
+            min = Infinity;
+            for (y = 0; y < len; y += 2) {
+                dy = vector[y];
+                vector[y] = dd = this._min(dy, d0, dd, bx0, vector[y + 1]);
+                d0 = dy;
+                if (maxDistance >= 0) {
+                    min = Math.min(min, dd);
+                }
+            }
+            if (maxDistance >= 0 && min > maxDistance) {
                 return false;
             }
         }
